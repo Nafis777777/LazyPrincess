2ca228fb2c92c1e016bd4d0ce7a348adf1963e53
Nafis777777 <115471692+Nafis777777@users.noreply.github.com>
2024-09-30 19:05:51 +0600

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 # MongoDB information *
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "LazyDeveloper")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'cinebazaar')
 
 # LOG CHANNELS *
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
